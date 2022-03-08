b689e8dc266a925560839de7fff16e53dacfe221
Sakshi Dubey <100987186+dubey-sakshi0702@users.noreply.github.com>
2022-03-08 22:06:42 +0530

    improved quality and error resolved
    improved quality by testing and debugging using pylint

--- ttt.py
+++ ttt.py
@@ -1,34 +1,42 @@
+"""This module does blah blah."""
 import random
 def choose_first():
+    '''
+    chooses the player who plays first
+    '''
     if random.randint(0, 1) == 0:
         return 'Player 2'
-    else:
-        return 'Player 1'
+    return 'Player 1'
 def change_player(player):
+    '''
+    give turn to other player
+    '''
     if player=='Player 1':
         return 'Player 2'
-    else:
-        return 'Player 1'
+    return 'Player 1'
 def board_input(board):
+    '''
+    get player's move position
+    '''
     i=0
-    while i not in [1,2,3,4,5,7,8,9]:
+    while i not in [1,2,3,4,5,7,8,9] :
         i=int(input('enter a no 1 to 9\n'))
         if board[i]==' ':
             return i
-        else:
-            continue
+        i=0
 def player_input():
+    '''
+    lets player chose his/her's tile/marker
+    '''
     marker = ''
-    
-    while not (marker == 'X' or marker == 'O'):
+    while marker not in ('X', 'O'):
         marker = input('Player 1: Do you want to be X or O? ').upper()
-
     if marker == 'X':
         return ('X', 'O')
-    else:
-        return ('O', 'X')
 def display_board(board):
-
+    '''
+    display board
+    '''
     print('   |   |')
     print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
     print('   |   |')
@@ -38,40 +46,38 @@ def display_board(board):
     print('   |   |')
     print('-----------')
     print('   |   |')
-    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3]) 
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
     print('   |   |')
 def win(board,mark):
-    return ((board[7] == mark and board[8] == mark and board[9] == mark) or 
-    (board[4] == mark and board[5] == mark and board[6] == mark) or 
-    (board[1] == mark and board[2] == mark and board[3] == mark) or 
-    (board[7] == mark and board[4] == mark and board[1] == mark) or 
-    (board[8] == mark and board[5] == mark and board[2] == mark) or 
-    (board[9] == mark and board[6] == mark and board[3] == mark) or 
-    (board[7] == mark and board[5] == mark and board[3] == mark) or 
-    (board[9] == mark and board[5] == mark and board[1] == mark)) 
+    '''
+    checks if the player won
+    '''
+    return (board[7] == mark and board[8] == mark and board[9] == mark) or (board[4] == mark and board[5] == mark and board[6] == mark) or (board[1] == mark and board[2] == mark and board[3] == mark) or (board[7] == mark and board[4] == mark and board[1] == mark) or (board[8] == mark and board[5] == mark and board[2] == mark) or (board[9] == mark and board[6] == mark and board[3] == mark) or (board[7] == mark and board[5] == mark and board[3] == mark) or (board[9] == mark and board[5] == mark and board[1] == mark)
 boar1=['0','1','2','3','4','5','6','7','8','9']
 boar2=['$',' ',' ',' ',' ',' ',' ',' ',' ',' ']
-
 print('welcome to tic tac toe')
-t=input('want to play?(enter y/Y to play)')
-if t=='y' or t== 'Y':
+T=input('want to play?(enter y/Y to play)')
+while T in ('y','Y'):
     print("welcome players\n\n\n ")
-c=player_input()
-display_board(boar1)
-print("\n\n\n")
-t=1
-display_board(boar2)
-player=choose_first()
-
-while t<9:
-    if player=='Player 1':
-        a=0
-    else:
-        a=1
-    y=board_input(boar2)
-    boar2[y]=c[a]
+    c=player_input()
+    display_board(boar1)
+    print("\n\n\n")
+    K=1
     display_board(boar2)
-    if win(boar2,c[a])==True:
-       print(f'{player} won')
-       break
-    player=change_player(player)
+    PLAYER=choose_first()
+    while K<=9:
+        if PLAYER=='Player 1':
+            A=0
+        else:
+            A=1
+        boar2[board_input(boar2)]=c[A]
+        display_board(boar2)
+        if win(boar2,c[A]):
+            print(f'{PLAYER} won')
+            break
+        PLAYER=change_player(PLAYER)
+        K+=1
+    else:
+        print("the game was a draw")
+    T=input('want to play again?(enter y/Y to play)')
+print('exit')
